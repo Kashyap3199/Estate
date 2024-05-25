2fa5aaecb242f9592ca5aeab8dbc39420685cd54
Your Name <you@example.com>
2024-05-25 17:25:42 -0500

    header component

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,18 @@ import About from "./pages/About";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/profile" element={<Profile />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/signin" element={<Signin />}></Route>
-        <Route path="/signout" element={<Signup />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
       </Routes>
     </BrowserRouter>
   );
